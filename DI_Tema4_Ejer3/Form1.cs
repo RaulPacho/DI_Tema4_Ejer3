5946e70b419774d0faafab0731c1ef40d550ec5e
Raul Pacho <raulpachovivas@gmail.com>
2020-11-26 12:59:29 +0100

    Finalizado

--- DI_Tema4_Ejer3/Form1.cs
+++ DI_Tema4_Ejer3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,52 @@ namespace DI_Tema4_Ejer3
 {
     public partial class Form1 : Form
     {
+        string path = "";
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Visor de Imagenes";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.DefaultExt = "JPG (.jpg) | *.jpg |PNG (.png)| *.png";
+           
+            oFD.ShowDialog();
+
+            path = oFD.FileName;
+
+            if (File.Exists(path))
+            {
+                Image i = Image.FromFile(path);
+                Form f2 = new Form();
+                f2.BackgroundImage = i;
+                f2.SetBounds(0,0,i.Width,i.Height);
+                f2.BackgroundImageLayout = ImageLayout.Stretch;
+                f2.Activate();
+                FileInfo fi = new FileInfo(path);
+                f2.Text =  fi.Name;
+                if (checkBox1.Checked)
+                {
+                    f2.ShowDialog();
+                }
+                else
+                {
+                    f2.Show();
+                }
+                
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           if( MessageBox.Show("Te piras?", "De que vas pavo?", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+            
         }
     }
 }
